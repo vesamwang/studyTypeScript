00f7fd4fdf35e749054b9198ea6d0e041d8e8268
levin <3259963@qq.com>
2016-11-22 21:37:55 +0800

    进一步理解enum

--- src/types.ts
+++ src/types.ts
@@ -5,11 +5,17 @@
 export enum DateType {Day, Week, Month}
 
 export interface UnitType {
-  key: string;
-  value: string;
+    key: string;
+    value: string;
 }
 
 export let DateTypes: UnitType[] = [];
 DateTypes[DateType.Day] = {key: 'D', value: 'day'};
 DateTypes[DateType.Week] = {key: 'W', value: 'week'};
 DateTypes[DateType.Month] = {key: 'M', value: 'month'};
+
+console.log(DateType.Day);
+console.log(DateType[DateType.Day]);
+console.log(DateType["Day"]);
+const w: any = "Week";
+console.log(DateType[w]);
